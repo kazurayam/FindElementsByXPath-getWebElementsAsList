22cc2fa73b4661b0953f2ad56e13250a9fe5931b
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-06-26 12:51:42 +0900

    first commit

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,27 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import java.lang.String
+
+
+def static "com.kazurayam.ksbackyard.FindElementsByXPath.getWebElementsAsList"(
+    	String xpath4elements	) {
+    (new com.kazurayam.ksbackyard.FindElementsByXPath()).getWebElementsAsList(
+        	xpath4elements)
+}
+
+def static "com.kazurayam.ksbackyard.FindElementsByXPath.getElementContentsAsList"(
+    	String xpath4elements	) {
+    (new com.kazurayam.ksbackyard.FindElementsByXPath()).getElementContentsAsList(
+        	xpath4elements)
+}
+
+def static "com.kazurayam.ksbackyard.FindElementsByXPath.getAttributeValuesAsList"(
+    	String xpath4elements	
+     , 	String attributeName	) {
+    (new com.kazurayam.ksbackyard.FindElementsByXPath()).getAttributeValuesAsList(
+        	xpath4elements
+         , 	attributeName)
+}
